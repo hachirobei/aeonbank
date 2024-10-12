ed7b18b7370b1b2444b19bf14dc1b7f08ad5c87d
hachirobei <hachirobei93@gmail.com>
2024-10-12 23:20:40 +0800

    added

--- librarysystem/src/main/java/com/aeon/librarysystem/LibrarySystemApplication.java
+++ librarysystem/src/main/java/com/aeon/librarysystem/LibrarySystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibrarysystemApplication {
+public class LibrarySystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibrarysystemApplication.class, args);
+		SpringApplication.run(LibrarySystemApplication.class, args);
 	}
 
 }
